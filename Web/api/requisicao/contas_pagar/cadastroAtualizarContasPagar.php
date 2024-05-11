3800d7e1fa00ea9de2b2e145fab15cb4acfb2de4
Caique Rawos <Caique.rawos@gmail.com>
2024-05-11 13:13:54 -0300

    Finalizando tela de contas a pagar

--- Web/api/requisicao/contas_pagar/cadastroAtualizarContasPagar.php
+++ Web/api/requisicao/contas_pagar/cadastroAtualizarContasPagar.php
@@ -1,40 +1,57 @@
 <?php
-// Configuração do banco de dados
-$host = 'localhost';
-$dbname = 'pagamentos';
-$username = 'root';
-$password = ' ';
 
-// Conectar ao banco de dados com tratamento de exceções
-try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+require_once  '../../../api/path/api-path.php';
+$url = API_PATH . 'contas_pagar/';
+
+function msgHttpCode($httpcode, $msg){
+http_response_code($httpcode);
+echo $msg;
+exit;
 }
 
-// Verificar se os campos obrigatórios estão definidos
-if (isset($_POST['descricao'], $_POST['valorTotal'], $_POST['dataVencimento'], $_POST['id_status_pagamento'], $_POST['id_forma_pagamento'])) {
-    $descricao = $_POST['descricao'];
-    $valorTotal = floatval($_POST['valorTotal']);
-    $dataVencimento = $_POST['dataVencimento'];
-    $id_status_pagamento = intval($_POST['id_status_pagamento']);
-    $id_forma_pagamento = intval($_POST['id_forma_pagamento']);
-
-    try {
-        // Preparar a consulta SQL
-        $stmt = $pdo->prepare("INSERT INTO pagamentos (descricao, valorTotal, dataVencimento, id_status_pagamento, id_forma_pagamento) 
-                               VALUES (?, ?, ?, ?, ?)");
-
-        // Executar a consulta com os parâmetros recebidos
-        $stmt->execute([$descricao, $valorTotal, $dataVencimento, $id_status_pagamento, $id_forma_pagamento]);
-
-        // Exibir mensagem de sucesso
-        echo "Pagamento registrado com sucesso!";
-    } catch (PDOException $e) {
-        echo "Erro ao inserir dados no banco de dados: " . $e->getMessage();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $codigo = isset($_POST["codigo"]) ? $_POST['codigo'] : '';
+    $descricao = isset($_POST["descricao"]) ? $_POST['descricao'] : msgHttpCode(400, 'Descrição não pode estar vazio');
+    $valorTotal = isset($_POST["valorTotal"]) && floatval($_POST["valorTotal"]) > 0 ? $_POST["valorTotal"] : msgHttpCode(400, 'Valor Total precisa ser maior que 0');
+    $valorPago = isset($_POST["valorPago"]) ? $_POST["valorPago"] : msgHttpCode(400, 'Valor Pago não pode estar vazio');
+    $dataVencimento = isset($_POST["dataVencimento"]) ? $_POST["dataVencimento"] : msgHttpCode(400, 'Data Vencimento não pode estar vazia');
+    $id_forma_pagamento = isset($_POST["id_forma_pagamento"]) ? intval($_POST["id_forma_pagamento"]) : msgHttpCode(400, 'Forma Pagamento não pode estar vazia');
+    $id_status_pagamento = isset($_POST["id_status_pagamento"]) ? intval($_POST["id_status_pagamento"]) : msgHttpCode(400, 'Status Pagamento não pode estar vazia');
+    
+    $data = array(
+        'descricao' => $descricao,
+        'valorTotal' => $valorTotal,
+        'valorPago' => $valorPago,
+        'dataVencimento' => $dataVencimento,
+        'idPagamento' => $id_forma_pagamento,
+        'idStatusPagamento' => $id_status_pagamento,
+      );
+    
+      $json_data = json_encode($data);
+    
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($json_data))
+      );
+      $response = curl_exec($ch);
+      $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      curl_close($ch);
+    
+      if ($httpcode == 201) {
+        http_response_code(201);
+        $responseData = json_decode($response, true);
+        if ($responseData !== null) {
+            echo json_encode($responseData);
+        }
+      } else {
+        msgHttpCode($httpcode, "Erro ao inserir Conta a Pagar: " . $response);
+      }
+    
+    }else{
+      msgHttpCode(400, "Permissão Negada");
     }
-} else {
-    echo "Por favor, preencha todos os campos obrigatórios.";
-}
 ?>
